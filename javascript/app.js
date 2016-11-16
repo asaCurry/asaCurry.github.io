d2b708fe3a005aa6786eac39b5656695a3309fed
Asa Curry <asacurry80@gmail.com>
2016-11-16 13:57:20 -0500

    js update 1

---
+++ javascript/app.js
@@ -0,0 +1,17 @@
+$( ".container-project-ideahack" ).mouseover(function() {
+  $('.container-project-ideahack').find('.project-container').removeClass("hidden");
+  $('.container-project-ideahack').find('.project-container').addClass("animated fadeInLeft");
+  console.log("class added");
+});
+
+
+
+function myFunction() {
+	$('.container-project-ideahack').find('.project-container').removeClass("hidden");
+	$('.container-project-ideahack').find('.project-container').addClass("animated fadeInLeft");
+	console.log("class added");
+}
+
+
+
+
