83505560438fb7de433d31fccc34279d52c9a5ea
Asa Curry <asacurry80@gmail.com>
2016-12-13 11:50:38 -0500

    pre expo update

--- javascript/app.js
+++ javascript/app.js
@@ -1,14 +1,14 @@
-$( ".container-project-ideahack" ).mouseover(function() {
-  $('.container-project-ideahack').find('.project-container').removeClass("hidden");
-  $('.container-project-ideahack').find('.project-container').addClass("animated fadeInLeft");
+$( ".large-container-project" ).mouseover(function() {
+  $('.large-container-project').find('.project-container').removeClass("hidden");
+  $('.large-container-project').find('.project-container').addClass("animated fadeInLeft");
   console.log("class added");
 });
 
 
 
 function myFunction() {
-	$('.container-project-ideahack').find('.project-container').removeClass("hidden");
-	$('.container-project-ideahack').find('.project-container').addClass("animated fadeInLeft");
+	$('.large-container-project').find('.project-container').removeClass("hidden");
+	$('.large-container-project').find('.project-container').addClass("animated fadeInLeft");
 	console.log("class added");
 }
 
